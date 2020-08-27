37b05ead53e0632a5d00eb2c1d28ec48ff97db1e
kapil-infobeans <kapil.yadav@infobeans.com>
2020-08-27 22:05:22 +0530

    customer registration and login

--- resources/views/customers/orders.blade.php
+++ resources/views/customers/orders.blade.php
@@ -14,15 +14,6 @@
                      </div>
                      <div>
                         <p class="py-3 h6">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                          onclick="event.preventDefault();
-      document.getElementById('logout-form').submit();">
-                                          <i class="fa fa-sign-out" aria-hidden="true"></i> {{ __('Logout') }}
-                                       </a>
-
-                                       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                          @csrf
-                                       </form>
                         </p>
                      </div>
                   </div>
