102323b2043d7f112d588a379e5d984107917465
kapil-infobeans <kapil.yadav@infobeans.com>
2020-09-18 18:00:40 +0530

    Added more templates

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -41,80 +41,80 @@
         <div class="row mb-5">
           <div class="col-md-4">
             <div class="post-entry">
-              <a href="blog-single.html" class="d-block mb-4">
+              <a href="{{ url('/blogsingle') }}" class="d-block mb-4">
                 <img src="assets/img/img_1.jpg" alt="Image" class="img-fluid">
               </a>
               <div class="post-text">
                 <span class="post-meta">December 13, 2019 &bullet; By <a href="#">Admin</a></span>
-                <h3><a href="#">Chrome now alerts you when someone steals your password</a></h3>
+                <h3><a href="{{ url('/blogsingle') }}">Chrome now alerts you when someone steals your password</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                <p><a href="#" class="readmore">Read more</a></p>
+                <p><a href="{{ url('/blogsingle') }}" class="readmore">Read more</a></p>
               </div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="post-entry">
-              <a href="blog-single.html" class="d-block mb-4">
+              <a href="{{ url('/blogsingle') }}" class="d-block mb-4">
                 <img src="assets/img/img_2.jpg" alt="Image" class="img-fluid">
               </a>
               <div class="post-text">
                 <span class="post-meta">December 13, 2019 &bullet; By <a href="#">Admin</a></span>
-                <h3><a href="#">Chrome now alerts you when someone steals your password</a></h3>
+                <h3><a href="{{ url('/blogsingle') }}">Chrome now alerts you when someone steals your password</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                <p><a href="#" class="readmore">Read more</a></p>
+                <p><a href="{{ url('/blogsingle') }}" class="readmore">Read more</a></p>
               </div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="post-entry">
-              <a href="blog-single.html" class="d-block mb-4">
+              <a href="{{ url('/blogsingle') }}" class="d-block mb-4">
                 <img src="assets/img/img_3.jpg" alt="Image" class="img-fluid">
               </a>
               <div class="post-text">
                 <span class="post-meta">December 13, 2019 &bullet; By <a href="#">Admin</a></span>
-                <h3><a href="#">Chrome now alerts you when someone steals your password</a></h3>
+                <h3><a href="{{ url('/blogsingle') }}">Chrome now alerts you when someone steals your password</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                <p><a href="#" class="readmore">Read more</a></p>
+                <p><a href="{{ url('/blogsingle') }}" class="readmore">Read more</a></p>
               </div>
             </div>
           </div>
 
           <div class="col-md-4">
             <div class="post-entry">
-              <a href="blog-single.html" class="d-block mb-4">
+              <a href="{{ url('/blogsingle') }}" class="d-block mb-4">
                 <img src="assets/img/img_4.jpg" alt="Image" class="img-fluid">
               </a>
               <div class="post-text">
                 <span class="post-meta">December 13, 2019 &bullet; By <a href="#">Admin</a></span>
-                <h3><a href="#">Chrome now alerts you when someone steals your password</a></h3>
+                <h3><a href="{{ url('/blogsingle') }}">Chrome now alerts you when someone steals your password</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                <p><a href="#" class="readmore">Read more</a></p>
+                <p><a href="{{ url('/blogsingle') }}" class="readmore">Read more</a></p>
               </div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="post-entry">
-              <a href="blog-single.html" class="d-block mb-4">
+              <a href="{{ url('/blogsingle') }}" class="d-block mb-4">
                 <img src="assets/img/img_3.jpg" alt="Image" class="img-fluid">
               </a>
               <div class="post-text">
                 <span class="post-meta">December 13, 2019 &bullet; By <a href="#">Admin</a></span>
-                <h3><a href="#">Chrome now alerts you when someone steals your password</a></h3>
+                <h3><a href="{{ url('/blogsingle') }}">Chrome now alerts you when someone steals your password</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                <p><a href="#" class="readmore">Read more</a></p>
+                <p><a href="{{ url('/blogsingle') }}" class="readmore">Read more</a></p>
               </div>
             </div>
           </div>
           <div class="col-md-4">
             <div class="post-entry">
-              <a href="blog-single.html" class="d-block mb-4">
+              <a href="{{ url('/blogsingle') }}" class="d-block mb-4">
                 <img src="assets/img/img_2.jpg" alt="Image" class="img-fluid">
               </a>
               <div class="post-text">
                 <span class="post-meta">December 13, 2019 &bullet; By <a href="#">Admin</a></span>
-                <h3><a href="#">Chrome now alerts you when someone steals your password</a></h3>
+                <h3><a href="{{ url('/blogsingle') }}">Chrome now alerts you when someone steals your password</a></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                <p><a href="#" class="readmore">Read more</a></p>
+                <p><a href="{{ url('/blogsingle') }}" class="readmore">Read more</a></p>
               </div>
             </div>
           </div>
